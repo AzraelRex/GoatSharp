69d262580e01f9f1ad026d10da89fa509b8f0e75
Azrael Rex Anacleto <anacletoazraelrex@gmail.com>
2024-04-03 01:12:20 +0800

    Added A dialogue and a new character

--- C#Practice 1/Program.cs
+++ C#Practice 1/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace C_Practice_1
+﻿using System.Diagnostics.Metrics;
+
+namespace C_Practice_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Homeboy!\n");
+            Console.WriteLine("His Homeboy:");
             Console.WriteLine("Yo whats poppin man?");
         }
     }
